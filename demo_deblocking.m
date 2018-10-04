63ff3253cdf7a7042a30043fa2ca5dded4e803d7
ऋतुराज गावस्कर <rgavaska@users.noreply.github.com>
2018-10-04 12:19:12 +0530

    Update demo_deblocking.m

--- demo_deblocking.m
+++ demo_deblocking.m
@@ -3,13 +3,13 @@ clearvars; close all;
 clc;
 
 input_image = './house.jpg'; 
-sigma_s = 3;
+rho = 3;
 N = 5;      % Order of polynomial
 
 f = imread(input_image);
 f = double(f);
 sigma_r = sigmaJPEG(f,15,1);
-g_hat = fastABF(f,sigma_s,sigma_r,[],N);
+g_hat = fastABF(f,rho,sigma_r,[],N);
 
 figure, imshow(uint8(f)); title('Input');  drawnow; pause(0.01);
 figure, imshow(uint8(g_hat)); title('Output');  drawnow; pause(0.01);
